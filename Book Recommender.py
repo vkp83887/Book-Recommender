e379af56d14d3cd19731be0bea303ad29863ada6
vkp83887 <132395604+vkp83887@users.noreply.github.com>
2023-08-27 21:49:36 +0530

    upload

---
+++ Book Recommender.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,request
+import pandas as pd
+import pickle
+import numpy as np
+
+popular_df = pd.read_pickle(open('popular.pkl','rb'))
+pt = pd.read_pickle(open('pt.pkl','rb'))
+books = pd.read_pickle(open('books.pkl','rb'))
+similarity_score = pd.read_pickle(open('similarity_score.pkl','rb'))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name =list(popular_df['Book-Title'].values),
+                           author =list(popular_df['Book-Author'].values),
+                           image =list(popular_df['Image-URL-M'].values),
+                           votes =list(popular_df['num_rating'].values),
+                           rating =list(popular_df['avg_rating'].values)
+                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books' ,methods=['POST'])
+def recommend():
+    user_input=request.form.get('user_input')
+    index=np.where(pt.index==user_input)[0][0]
+    similar_books=sorted(list(enumerate(similarity_score[index])),key=lambda x:x[1],reverse=True)[1:6]
+    
+    data=[]
+    for i in similar_books:
+      item=[]
+      temp_df= books[books['Book-Title'] == pt.index[i[0]]]
+      item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+      item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+      item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+  
+      data.append(item)
+
+    return render_template('recommend.html',data=data)
+if __name__ == '__main__':
+    app.run(debug=True)
